58c38dedd9eca21ea7b411631ea6943a81ce4cf4
Sckroll <kimsc0714@gmail.com>
2023-09-30 15:28:52 +0900

    타일 테두리 및 주석 추가

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -3,19 +3,25 @@ import * as PIXI from 'pixi.js';
 import { Graphics } from '@pixi/react';
 import { TILE_SIZE } from '../config';
 
+/**
+ * 테트로미노를 이루는 타일(사각형)을 관리하는 컴포넌트
+ */
 export default function Tile({ x, y, color }: TileProps) {
   const draw = useCallback(
     (g: PIXI.Graphics) => {
       g.clear();
 
+      // 흰색 배경 (타일 색상을 잘 보이게 하기 위함)
       g.beginFill('white');
       g.drawRect(x, y, TILE_SIZE, TILE_SIZE);
       g.endFill();
 
+      // 기본 색상
       g.beginFill(color, 0.5);
       g.drawRect(x, y, TILE_SIZE, TILE_SIZE);
       g.endFill();
 
+      // 삼각형 2개를 겹쳐서 X자 무늬 형성
       g.beginFill(color, 0.25);
       g.drawPolygon(x, y, x + TILE_SIZE, y + TILE_SIZE, x, y + TILE_SIZE);
       g.drawPolygon(
@@ -27,6 +33,10 @@ export default function Tile({ x, y, color }: TileProps) {
         y + TILE_SIZE,
       );
       g.endFill();
+
+      // 타일 테두리
+      g.lineStyle(1, color, 0.1);
+      g.drawRect(x, y, TILE_SIZE, TILE_SIZE);
     },
     [x, y, color],
   );
